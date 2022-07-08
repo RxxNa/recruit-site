381214ed2b2ffa22acd229ee43d23febb080d0d6
RxxNa <jho.7ly@gmail.com>
2022-07-08 17:35:43 +0900

    4th commit

---
+++ script.js
@@ -0,0 +1,17 @@
+// アコーディオン
+document.addEventListener('DOMContentLoaded', function () {
+  const accordionTitle = document.querySelectorAll('.accordionTitle');
+
+  for (let i = 0; i < accordionTitle.length; i++) {
+    accordionTitle[i].addEventListener("click", function () {
+      for (let j = 0; j < accordionTitle.length; j++) {
+        if (accordionTitle[i] !== accordionTitle[j]) {
+          accordionTitle[j].classList.remove("active");
+          accordionTitle[j].nextElementSibling.classList.remove("open");
+        }
+      }
+      this.classList.toggle('active');
+      this.nextElementSibling.classList.toggle('open');
+    });
+  }
+});
